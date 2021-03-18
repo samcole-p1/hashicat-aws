c695727b637a3673f8a8f56ac12a111522f945df
Sam Cole <sam@p1technologies.com>
2021-03-18 18:46:34 +0000

    added s3 bucket

---
+++ s3-bucket.tf
@@ -0,0 +1,10 @@
+
+
+module "s3-bucket" {
+  source  = "app.terraform.io/samcole-training/s3-bucket/aws"
+  version = "1.17.0"
+  # insert required variables here
+  bucket_prefix = var.prefix
+  bucket = = "hashicat"
+}
+
