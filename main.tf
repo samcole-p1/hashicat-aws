8d4627f4b0fe68511fee8f92aecbd30366100a22
Sam Cole <27911085+samcole-p1@users.noreply.github.com>
2021-03-26 13:46:59 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,12 +27,12 @@ resource "aws_security_group" "hashicat" {
 
   vpc_id = aws_vpc.hashicat.id
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  #ingress {
+  #  from_port   = 22
+  #  to_port     = 22
+  #  protocol    = "tcp"
+  #  cidr_blocks = ["0.0.0.0/0"]
+  #}
 
   #ingress {
   #  from_port   = 80
